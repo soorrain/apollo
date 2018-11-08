f6c036fb0ea43f753762e8a053c2107d44036a20
laiyongle <laiyongle@souche.com>
2018-11-08 14:47:40 +0800

    Config Service 配置读取接口

--- apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/service/config/AbstractConfigService.java
+++ apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/service/config/AbstractConfigService.java
@@ -20,6 +20,7 @@ public abstract class AbstractConfigService implements ConfigService {
   @Override
   public Release loadConfig(String clientAppId, String clientIp, String configAppId, String configClusterName,
       String configNamespace, String dataCenter, ApolloNotificationMessages clientMessages) {
+    // 优先，获得指定 Cluster 的 Release 。若存在，直接返回。
     // load from specified cluster fist
     if (!Objects.equals(ConfigConsts.CLUSTER_NAME_DEFAULT, configClusterName)) {
       Release clusterRelease = findRelease(clientAppId, clientIp, configAppId, configClusterName, configNamespace,
@@ -30,6 +31,8 @@ public abstract class AbstractConfigService implements ConfigService {
       }
     }
 
+    // 其次，获得所属 IDC 的 Cluster 的 Release 。若存在，直接返回
+    // TODO !Objects.equals(dataCenter, configClusterName) 这个判断用处？
     // try to load via data center
     if (!Strings.isNullOrEmpty(dataCenter) && !Objects.equals(dataCenter, configClusterName)) {
       Release dataCenterRelease = findRelease(clientAppId, clientIp, configAppId, dataCenter, configNamespace,
@@ -39,6 +42,7 @@ public abstract class AbstractConfigService implements ConfigService {
       }
     }
 
+    // 最后，获得默认 Cluster 的 Release 。
     // fallback to default release
     return findRelease(clientAppId, clientIp, configAppId, ConfigConsts.CLUSTER_NAME_DEFAULT, configNamespace,
         clientMessages);
@@ -57,15 +61,18 @@ public abstract class AbstractConfigService implements ConfigService {
    */
   private Release findRelease(String clientAppId, String clientIp, String configAppId, String configClusterName,
       String configNamespace, ApolloNotificationMessages clientMessages) {
+    // 读取灰度发布编号
     Long grayReleaseId = grayReleaseRulesHolder.findReleaseIdFromGrayReleaseRule(clientAppId, clientIp, configAppId,
         configClusterName, configNamespace);
 
+    //  读取灰度 Release 对象
     Release release = null;
 
     if (grayReleaseId != null) {
       release = findActiveOne(grayReleaseId, clientMessages);
     }
 
+    // 非灰度，获得最新的，并且有效的 Release 对象
     if (release == null) {
       release = findLatestActiveRelease(configAppId, configClusterName, configNamespace, clientMessages);
     }
